d0172aeed8ed303d837cc09fa82ad0cfaf67cc10
ziegler121 <asieduj999@gmail.com>
2022-10-13 06:01:20 -0700

    prints exactly "Programming is like building a multilingual puzzle, followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+  *main - entry point
+  *
+  *Return: 0 after printing the string
+  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
