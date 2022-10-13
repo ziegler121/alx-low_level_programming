3f53c32f46ce4bda562ed13e5348afa7eb354550
ziegler121 <asieduj999@gmail.com>
2022-10-13 09:29:55 -0700

    Prints a string without using printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  *main - entry point to entire program
+  *Description: prints a string without using
+  *printf or puts
+  *Return: 1 upon success
+  */
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
