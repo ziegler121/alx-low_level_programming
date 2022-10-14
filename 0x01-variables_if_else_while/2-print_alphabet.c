86aac1f42ea5638e30b2d2531ef35d9e93af92a1
ziegler121 <asieduj999@gmail.com>
2022-10-14 09:02:59 -0700

    prints the alphabet in lowercase, followed by a new line using putchar twice

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  *main - entry point into code
+  *
+  *Description: prints the alphabet in lowercase, followed
+  *by a new line using putchar function only twice
+  *
+  *Return: 0 upon success
+  */
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (int i = 0; i < 26; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
