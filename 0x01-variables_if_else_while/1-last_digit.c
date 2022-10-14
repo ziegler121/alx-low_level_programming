c102e5cb9cedbe9fce3593e11efa7e71924dbd0a
ziegler121 <asieduj999@gmail.com>
2022-10-14 08:35:51 -0700

    prints the last digit of the number stored in the variable n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0")
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, lastnum);
 	}
 	return (0);
 }
