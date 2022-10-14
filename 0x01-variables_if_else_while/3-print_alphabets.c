b8e1dfce55e2f13c108a393baa135b4b690e3724
ziegler121 <asieduj999@gmail.com>
2022-10-14 09:18:25 -0700

    prints the alphabet in lowercase, and then in uppercase, followed by a new line using putchar only three times

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+  *main - entry point into the code
+  *
+  *Descsription: prints the alphabet in lowercase,and then 
+  *in uppercase, follwed by a new line
+  *
+  *Return: 0 upon success
+  */
+int main(void)
+{
+	char lower[] = "abcdefghijklmnopqrstuvwxyz";
+	char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(lower[i]);
+	}
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(upper[i]);
+	}
+
+	putchar('\n');
+	return (0);
+}
