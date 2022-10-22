91a793ab5137f38fdfd136d653d4fd0ecc2cdf26
ziegler121 <asieduj999@gmail.com>
2022-10-21 22:55:50 -0700

    draws a straight line in the terminal.

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,17 +4,16 @@
  * print_line - draws a straight line in the terminal
  * @n: number of times the character _ should be printed
  */
-
 void print_line(int n)
 {
-
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}else
+	}
+	else
 	{
 		int i;
-	
+
 		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
