8898f72672fccc747a28f1ce37f016139d6ece3f
ziegler121 <asieduj999@gmail.com>
2022-10-24 15:35:16 -0700

    prints n elements of an array of integers, followed by a new line.

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,7 +5,7 @@
  *print_array - prints n elements of an array of integers
  *followed by a new line
  *@a: target array
- @n: number of elements of the array to be printed
+ *@n: number of elements of the array to be printed
  */
 
 void print_array(int *a, int n)
